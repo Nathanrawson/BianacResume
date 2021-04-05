269675eb46abd599806a829f419c436322c8dfcb
Nathan <nathan.rawson@downlo-dev.com>
2021-04-05 13:49:00 +0100

    fix redirect

--- src/components/BottomLinks.js
+++ src/components/BottomLinks.js
@@ -4,10 +4,10 @@ const BottomLinks = () => {
 
     return (<div id="HomeLinks">
         <div>
-            <h3><p className="minerva-font" onClick={() => { window.open("https://www.instagram.com/biancaxfranco") }}
-            >Instagram</p>&#65279; / <p className="minerva-font"
-                onClick={() => { window.open("https://www.tiktok.com/@biancaxfranco?lang=en") }}>Tiktok</p> / <p className="minerva-font"
-                    onClick={() => { window.open("https://www.linkedin.com/in/bianca-franco-97a72910a/") }}>Linkedin</p>
+            <h3><span className="minerva-font" onClick={() => { window.open("https://www.instagram.com/biancaxfranco") }}
+            >Instagram</span>&#65279; / <span className="minerva-font"
+                onClick={() => { window.open("https://www.tiktok.com/@biancaxfranco?lang=en") }}>Tiktok</span> / <span className="minerva-font"
+                    onClick={() => { window.open("https://www.linkedin.com/in/bianca-franco-97a72910a/") }}>Linkedin</span>
             </h3>
         </div>
     </div>)
