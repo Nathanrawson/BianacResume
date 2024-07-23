12f955fe352c48c072cf90c9304216770b30cf4c
Nathan Rawson <39409572+Nathanrawson@users.noreply.github.com>
2024-07-23 17:30:30 -0400

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,7 +4,7 @@ import BottomLinks from './BottomLinks'
 import './App.css';
 
 const Portfolio = (props) => {
-    return (<div><div id="PortfolioContent"><div><h3>Portfolio</h3><PortfolioSquare id="Portfolio1" title="University Of East London Social Media"></PortfolioSquare ><PortfolioSquare id="Portfolio2" title="Student Beans Instagram"></PortfolioSquare><PortfolioSquare id="Portfolio3" title="No Guilt Bakes Instagram"></PortfolioSquare></div>  <div id="HomeLinks">
+    return (<div><div id="PortfolioContent"><div><h3>Portfolio</h3><PortfolioSquare id="Portfolio1" title="University Of East London Social Media"></PortfolioSquare ><PortfolioSquare id="Portfolio2" title="Student Beans Instagram"></PortfolioSquare><PortfolioSquare id="Portfolio3" title="No Guilt Bakes Instagram"></PortfolioSquare><PortfolioSquare id="Portfolio4" title="OneDrive X (Twitter)"></PortfolioSquare><PortfolioSquare id="Portfolio5" title="Microsoft Surface X (Twitter)"></PortfolioSquare><PortfolioSquare id="Portfolio6" title="Bonza TikTok"></PortfolioSquare><PortfolioSquare id="Portfolio7" title="Dunkin' Donuts UK"></PortfolioSquare></div>  <div id="HomeLinks">
 
     </div>  </div> <BottomLinks /> </div>)
 }
